33e554c0509520da9c0ff5a5b18f6ff217e7b418
Timur <timur.ochakovsky@gmail.com>
2017-09-08 18:00:20 -0400

    smooth scrolling & tiny png

--- js/run.js
+++ js/run.js
@@ -1,53 +1,54 @@
 $(document).ready(function() {
 
   
-  // 1. blur cover-image on page load
-  var divs = $('.cover_img');
+    // 1. blur cover-image on page load
+    var divs = $('.cover_img');
 
-  divs.each(function() {
-    var elem = $(this);
-    var imgSrc = elem.attr('data-image-large');
+    divs.each(function() {
+      var elem = $(this);
+      var imgSrc = elem.attr('data-image-large');
 
-    if (imgSrc) {
-      var img = new Image();
-      $(img).load(function() {
+      if (imgSrc) {
+        var img = new Image();
+        $(img).load(function() {
 
-        console.log(elem.css('backgroundImage'));
+          console.log(elem.css('backgroundImage'));
 
-        elem.css('backgroundImage', 'url(' + imgSrc + ')');
-        elem.addClass('hide-blur');
-      });
+          elem.css('backgroundImage', 'url(' + imgSrc + ')');
+          elem.addClass('hide-blur');
+        });
 
-      img.src = imgSrc;
-    }
-
-
-
-    // 2. run slider in the model  
-    baguetteBox.run(".gallery");
+        img.src = imgSrc;
+      }
 
 
-    // 3. hide arrow
-    function func_show() {
-      $(".arrow").removeClass("hidden");
-    }
-    setTimeout(func_show, 4000)
-  	});
+
+      // 2. run slider in the model  
+      baguetteBox.run(".gallery");
+
+
+      // 3. hide arrow
+      function func_show() {
+        $(".arrow").removeClass("hidden");
+      }
+      setTimeout(func_show, 4000)
+    	});
 
 
   	
 
-  	 // 4. testing smooth scroll
-  	 $(".smooth-scroll").click(function() {
-       // $(".parallax").css("overflow-x", "disable" );
-       //  $(".parallax").css("overflow-y", "disable" );
-      var href = $(this).attr('href');
+    	 // 4. testing smooth scroll
+    	 $(".smooth-scroll").click(function() {
         
-        $('body').animate({
-            scrollTop: $(href).offset().top
-        }, 8800);
-        console.log(3);
-    });
+        var href = $(this).attr('href');
+          
+          $('.parallax').animate({
+              scrollTop: $(href).offset().top
+          }, 2500);
+      });
+
+
+  
 
 
 
